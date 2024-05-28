ad08b0fafc539d94e897bcf4580447a2620e38e8
edd-ie <ogendoeddie@gmail.com>
2024-05-28 18:34:20 -0300

    Game window initialization

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "raylib.h"
+
+using namespace std;
+
+// Game theme:
+Color green = {173,204,96,255};
+Color darkGreen = {43, 51, 24, 255};
+
+// Game grid:
+int cellSize = 30;
+int cellCount = 24;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Vintage Snake");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(green);
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
