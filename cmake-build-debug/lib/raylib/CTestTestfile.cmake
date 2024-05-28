6ce8e524c0f4fbdac1a5e993b95bed5153e23441
edd-ie <ogendoeddie@gmail.com>
2024-05-28 18:33:20 -0300

    Raylib Configuration

---
+++ cmake-build-debug/lib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: X:/Code/C++/SnakeRaylib/lib/raylib
+# Build directory: X:/Code/C++/SnakeRaylib/cmake-build-debug/lib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
---
+++ cmake-build-debug/lib/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: X:/Code/C++/SnakeRaylib/lib/raylib/src
+# Build directory: X:/Code/C++/SnakeRaylib/cmake-build-debug/lib/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
